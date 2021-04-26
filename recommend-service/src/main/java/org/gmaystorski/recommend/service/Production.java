80ce8b5c324ace776b234bc46f0fee23212153b8
Georgi Maystorski <georgi.maystorski@sap.com>
2021-04-26 21:18:50 +0300

    Fixed scoring algo

--- recommend-service/src/main/java/org/gmaystorski/recommend/service/Production.java
+++ recommend-service/src/main/java/org/gmaystorski/recommend/service/Production.java
@@ -4,40 +4,45 @@ import java.util.List;
 
 public class Production {
 
-	private String id;
-	private String description;
-	private String title;
-	private List<String> categories;
+    private String id;
+    private String description;
+    private String title;
+    private List<String> categories;
 
-	public Production(String id, String description, String title, List<String> categories) {
-		this.id = id;
-		this.description = description;
-		this.title = title;
-		this.categories = categories;
-	}
+    public Production(String id, String description, String title, List<String> categories) {
+        this.id = id;
+        this.description = description;
+        this.title = title;
+        this.categories = categories;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public List<String> getCategories() {
-		return categories;
-	}
+    public List<String> getCategories() {
+        return categories;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		if (other instanceof Production) {
-			return this.id.equals(((Production) other).id);
-		}
-		return super.equals(other);
-	}
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Production) {
+            return this.id.equals(((Production) other).id);
+        }
+        return super.equals(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
 
 }
--- recommend-service/src/main/java/org/gmaystorski/recommend/service/TestRunner.java
+++ recommend-service/src/main/java/org/gmaystorski/recommend/service/TestRunner.java
@@ -12,19 +12,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestRunner implements ApplicationRunner {
 
-	@Autowired
-	private RecommendService service;
+    @Autowired
+    private RecommendService service;
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		Map<Production, Double> recommendations = service.processInput(
-				Arrays.asList("Brad Pitt", "Charlie Cox", "Scarlett Johansson", "Robert Downey Jr.", "Chris Hemsworth"),
-				Arrays.asList("The Matrix", "Marvel's The Defenders", "Marvel's Daredevil", "Men in Black",
-						"Pulp Fiction", "American Psycho", "The Avengers"),
-				Arrays.asList("Action & Adventure", "Dramas", "TV Action & Adventure"));
-		recommendations.entrySet().stream().sorted(Comparator.comparingDouble(entry -> entry.getValue()))
-				.forEachOrdered(entry -> System.out.println(
-						"Recommended movie: " + entry.getKey().getTitle() + " with score: " + entry.getValue()));
-	}
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Map<Production, Double> recommendations = service.processInput(
+                Arrays.asList("Brad Pitt", "Charlie Cox", "Scarlett Johansson", "Robert Downey Jr.", "Chris Hemsworth"),
+                Arrays.asList("Friends",
+                        "Marvel's The Defenders",
+                        "Marvel's Daredevil",
+                        "The Office (U.S.)",
+                        "Pulp Fiction",
+                        "American Psycho"),
+                Arrays.asList("Action & Adventure", "Dramas", "Comedies", "Thrillers"));
+        recommendations.entrySet()
+                       .stream()
+                       .sorted(Comparator.comparingDouble((Map.Entry<Production, Double> entry) -> entry.getValue())
+                                         .reversed())
+                       .limit(10)
+                       .forEachOrdered(entry -> System.out.println(
+                               "Recommended movie: " + entry.getKey().getTitle() + " with score: " + entry.getValue()));
+    }
 
 }
